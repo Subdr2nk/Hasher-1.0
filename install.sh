e94257ec5479f14b00c414266af2fcbe6027a22b
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 20:13:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ set version = "1.0"
 source package_run.sh
 source build.sh
 echo "Building package_run.sh"
+sleep(2)
 pack
+echo "Sucessfully build package_run.sh"
+echo "Packing md5 toolkit"
+sleep(2)
+echo "Sucessfully Packed md5 toolkit"
 build_package
 
