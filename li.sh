e1b714f8816a8b537d8a2b4fe8a8944387cae08a
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 18:36:25 -0300

    Create li.sh

---
+++ li.sh
@@ -0,0 +1,17 @@
+touch LICENSE.txt
+touch README.md
+
+echo " 
+# dir (ls type)
+
+~/app/Hasher-1.0/
+            LICENSE
+             README.md
+             build.sh
+             gettat.sh
+             install.sh
+             package_get.sh
+             package_run.sh
+             
+
+" >> README.md
