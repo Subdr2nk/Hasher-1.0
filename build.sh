32c8eb9aaa3727d65043183ca64bb7c0ad01cd50
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 20:10:18 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,22 @@
-touch .gitignore
-touch setup.cfg
-touch .git
+function buildclass(){
+  echo "echo Insert something to hash." >> "Hasher 1.0.sh"
+  echo "read hashinput" >> "Hasher 1.0.sh"
+  echo "set hashFound = md5sum <<< hashinput" >> "Hasher 1.0.sh"
+  echo "Your hash: $hashfound " >> "Hasher 1.0.sh"
+}
+
+function build(){
+  touch .gitignore
+  touch setup.cfg
+  touch .git
+  touch .config
+  touch terminal.toml
+
+  if test -F ["/app"] then
+    cd app
+    touch "Hasher 1.0.sh"
+    buildclass
+  fi
+}
+
 
