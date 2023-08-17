b3c7d65b12488fec337964919723f6fe6f91c955
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 19:57:05 -0300

    Delete getat.sh

--- getat.sh
+++
@@ -1,7 +0,0 @@
-bash " build.sh"
-touch "Hasher 1.0.sh"
-
-echo "echo Insert something you want to hash" >> "Hasher 1.0.sh" 
-echo "readline hashtext" >>  "Hasher 1.0.sh"
-echo "set HashOutput = md5sum <<< hashtext" >> "Hasher 1.0.sh"
-echo "echo Your hash is: $hashoutput "
