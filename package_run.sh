0b350797176943c380f13ee50cf800c170ebc9c0
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 20:05:22 -0300

    Update package_run.sh

--- package_run.sh
+++ package_run.sh
@@ -1,5 +1,20 @@
-mkdir "app"
-set package_get = "package_get.sh"
-bash $package_get
-
-
+function pack(){
+  mkdir "app"
+  cd app 
+  mkdir infos
+  cd infos
+  touch sys32.dll
+  touch 32bits.dll
+  touch 64bits.dll
+  touch gitignore.dll
+  touch sys64.dll
+  touch ubuntu.dll
+  touch mac.dll
+  touch os.dll
+  touch __sys__.dll
+  touch __init__.dll
+  cd ..
+  cd ..
+  
+  
+}
