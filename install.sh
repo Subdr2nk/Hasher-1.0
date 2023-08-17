f9cf73b001e14b5070276e9a9a3616013d928c73
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 19:59:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,6 @@
 echo Collecting Files..
 set version = "1.0"
-set file = "package_run.sh"
-set pack_li = "li.sh"
+source package_run.sh
+source build.sh
 echo "Building package_run.sh"
 
-
-
-
-bash $file
-sleep 4
-
-echo Sucessfully Installed additional files
-
-
-
-echo "Downloading LICENSE & README.md"
-
-bash $pack_li
-
-
-
-echo "Sucessfully installed Hasher $version"
-
-sleep 5
