ab17409878d623c36aa1aac2f8383c2fb56d85f3
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 18:12:28 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,34 @@
-echo "Insert something you want to hash."
-read hashinput
-set HashOutput = md5sum <<< hashinput
-echo "Your hash is: $HashOutput"
+echo Collecting Files..
+set version = "1.0"
+set file = "package_run.sh"
+echo "Downloading package_run.sh"
+sleep(1)
+set otherfile = "package_get.sh"
+sleep(1)
+echo "Sucessfully downloaded package_get.sh"
+
+nullied = ($file , $otherfile)
+
+
+for i in ${nullied}; do
+  eval(i)
+done
+
+echo Sucessfully Installed additional files
+
+set package_needed = "getat.sh" 
+
+echo "Collecting package_needed.. "
+
+eval($package_needed)
+
+install_pack=("LICENSE.txt", "README.md")
+
+echo "collecting LICENSE & README.md"
+
+for i in ${install_pack}
+  eval(i)
+
+echo "Sucessfully installed Hasher $version"
+
+sleep(5)
