9cef2a923206dece50e97fd340811d7e480c6032
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 21:06:13 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ function buildclass(){
   echo "echo Insert something to hash." >> "Hasher 1.0.sh"
   echo "read hashinput" >> "Hasher 1.0.sh"
   echo "set hashFound = md5sum <<< hashinput" >> "Hasher 1.0.sh"
-  echo "Your hash: $hashfound " >> "Hasher 1.0.sh"
+  echo "Your hash: $hashFound " >> "Hasher 1.0.sh"
 }
 
 function build_package(){
