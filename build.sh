5b5fa1bd4ac5e2b416fa753fb8958fce419bf6ab
Subdr2nk <136206803+Subdr2nk@users.noreply.github.com>
2023-08-17 22:18:05 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ function buildclass(){
   echo "echo Insert something to hash." >> "Hasher 1.0.sh"
   echo "read hashinput" >> "Hasher 1.0.sh"
   echo "md5sum <<< hashinput" >> "Hasher 1.0.sh"
-  echo "sleep 6" >> "Haser 1.0.sh"
+  echo "sleep 6" >> "Hasher 1.0.sh"
 }
 
 function build_package(){
